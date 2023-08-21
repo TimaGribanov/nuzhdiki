a1ec44d4ccd3c77f572cfe9dcf9888b64fbd48cf
TimaGribanov <48593815+TimaGribanov@users.noreply.github.com>
2023-08-21 13:19:35 +0200

    fix workflow

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,39 @@
+const request = (path) => {
+  let httpRequest
+
+  const makeRequest = (path) => {
+      httpRequest = new XMLHttpRequest()
+
+      if (!httpRequest) {
+          alert('Giving up :( Cannot create an XMLHTTP instance')
+          return false
+      }
+      httpRequest.onreadystatechange = alertContents
+      httpRequest.open('GET', `http://localhost:5000/${path}`)
+      httpRequest.send()
+  }
+
+  const alertContents = () => {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+          if (httpRequest.status === 200) {
+              document.getElementById('neuro-res').innerText = httpRequest.responseText
+          } else {
+              alert('There was a problem with the request.')
+          }
+      }
+  }
+
+  makeRequest(path)
+}
+
 document.getElementById('btn-neuro').addEventListener('click', () => {
-  window.open('./neuro')
+  request('neuro')
 })
 
 document.getElementById('btn-random').addEventListener('click', () => {
-  window.open('./random')
+  request('random')
 })
 
 document.getElementById('btn-mixed').addEventListener('click', () => {
-  window.open('./mixed')
+  request('mixed')
 })
