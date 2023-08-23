0f1032c6e9b03665f31f868a59989246350f33b7
TimaGribanov <48593815+TimaGribanov@users.noreply.github.com>
2023-08-23 23:22:54 +0200

    copy buttons

--- frontend/script.js
+++ frontend/script.js
@@ -2,25 +2,25 @@ const request = (path) => {
   let httpRequest
 
   const makeRequest = (path) => {
-      httpRequest = new XMLHttpRequest()
+    httpRequest = new XMLHttpRequest()
 
-      if (!httpRequest) {
-          alert('Giving up :( Cannot create an XMLHTTP instance')
-          return false
-      }
-      httpRequest.onreadystatechange = alertContents
-      httpRequest.open('GET', `http://localhost:5000/${path}`)
-      httpRequest.send()
+    if (!httpRequest) {
+      alert('Giving up :( Cannot create an XMLHTTP instance')
+      return false
+    }
+    httpRequest.onreadystatechange = alertContents
+    httpRequest.open('GET', `https://nuzhdiki.timagribanov.com/api/${path}`)
+    httpRequest.send()
   }
 
   const alertContents = () => {
-      if (httpRequest.readyState === XMLHttpRequest.DONE) {
-          if (httpRequest.status === 200) {
-              document.getElementById(`${path}-res`).innerText = httpRequest.responseText
-          } else {
-              alert('There was a problem with the request.')
-          }
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        document.getElementById(`${path}-res`).innerText = httpRequest.responseText
+      } else {
+        alert('There was a problem with the request.')
       }
+    }
   }
 
   makeRequest(path)
@@ -36,4 +36,25 @@ document.getElementById('btn-random').addEventListener('click', () => {
 
 document.getElementById('btn-mixed').addEventListener('click', () => {
   request('mixed')
+})
+
+const copyText = (path) => {
+  let text = document.getElementById(`${path}-res`).innerText
+
+  navigator.clipboard.writeText(text).then(
+    () => console.log('Async: Copying to clipboard was successful!'),
+    (err) => console.error('Async: Could not copy text: ', err)
+  )
+}
+
+document.getElementById('copy-neuro').addEventListener('click', () => {
+  copyText('neuro')
+})
+
+document.getElementById('copy-random').addEventListener('click', () => {
+  copyText('random')
+})
+
+document.getElementById('copy-mixed').addEventListener('click', () => {
+  copyText('mixed')
 })
